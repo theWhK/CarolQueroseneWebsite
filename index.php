6e4a69616cbeddbce2495175638f9b2fa123a054
Willian Hideki <willhkblz@gmail.com>
2023-07-22 03:37:54 -0300

    Ajustes visuais

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 <body>
     <div class="container">
         <div class="row">
-            <div class="col-12">
+            <div class="col-12 text-center">
                 <img class="logotipo" src="assets/logotipo.png" alt="Carol">
             </div>
         </div>
